1c65274af8eda3579f68b79e6b5f96b0234dae4d
Benjamin Kan <Kan@ibm.com>
2023-07-27 17:41:03 -0700

    Initial commit

---
+++ src/main/java/de/b3nk4n/gamecloud/logisticservice/LogisticServiceApplication.java
@@ -0,0 +1,13 @@
+package de.b3nk4n.gamecloud.logisticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/b3nk4n/gamecloud/logisticservice/LogisticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.b3nk4n.gamecloud.logisticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
